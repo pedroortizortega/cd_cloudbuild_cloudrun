f60d81f675eb9cebae8c3fbc964e99e3e3a58429
PedroE_Ortiz <pedrortiz89@gmail.com>
2022-04-08 12:28:43 -0500

    Create server.js
    Creando el servidor para node

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log('New connection')
+    res.end('Hello cloud gurus')
+})
+
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => console.log('Listening'))
